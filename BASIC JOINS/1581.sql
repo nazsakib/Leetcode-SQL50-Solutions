781b91db2ce516296343b64d0c1dc2502a42572e
Sakib MD Nazmush <sakibnshakhawat@gmail.com>
2023-11-06 10:26:27 +0600

    query using join

---
+++ BASIC JOINS/1581.sql
@@ -0,0 +1,2 @@
+-- 1581. Customer Who Visited but Did Not Make Any Transactions
+SELECT Visits.customer_id, COUNT(Visits.visit_id) AS count_no_trans FROM Visits LEFT JOIN Transactions ON Visits.visit_id = Transactions.visit_id WHERE transaction_id IS NULL GROUP BY customer_id;
